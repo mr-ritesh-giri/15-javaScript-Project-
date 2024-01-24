dc1ba78af5385c7df3cfef7c8da124137f9b3851
Ritesh Giri <rrgiri032@gmail.com>
2024-01-24 13:49:13 +0530

    Challenge 8 on progress.

--- Challenge-7/script.js
+++ Challenge-7/script.js
@@ -3,11 +3,12 @@ let more = document.querySelectorAll(".see-more");
 more.forEach(function (button) {
   let faqBox = button.closest(".faq-box");
   let info = faqBox.querySelector(".info");
-  let horiLine = document.querySelector(".horizontal-line");
+  let plusBtn = faqBox.querySelector(".plus-btn");
+  let minusBtn = document.querySelector(".minus-btn");
 
   button.addEventListener("click", function () {
     info.classList.toggle("hide");
-    horiLine.classList.toggle("hide");
-    console.log(button, "Clicked");
+    minusBtn.classList.toggle("hide");
+    plusBtn.classList.toggle("hide");
   });
 });
