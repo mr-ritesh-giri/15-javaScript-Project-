d75940ed05f2f7c36ae2ca6b1dffec1759913252
Ritesh Giri <rrgiri032@gmail.com>
2024-01-24 21:26:19 +0530

    Frustrating.

--- Challenge-7/script.js
+++ Challenge-7/script.js
@@ -3,7 +3,7 @@ let more = document.querySelectorAll(".see-more");
 more.forEach(function (button) {
   let faqBox = button.closest(".faq-box");
   let info = faqBox.querySelector(".info");
-  let plusBtn = faqBox.querySelector(".plus-btn");
+  let plusBtn = document.querySelector(".plus-btn");
   let minusBtn = document.querySelector(".minus-btn");
 
   button.addEventListener("click", function () {
